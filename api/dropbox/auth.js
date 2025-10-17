765914d237438d8a03c00a45a646e2acc629c6e4
jaeo88 <jaeo88@users.noreply.github.com>
2025-10-18 00:44:39 +0900

    Fix Dropbox auth API route

--- api/dropbox/auth.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const clientId = process.env.DROPBOX_APP_KEY;
-  const redirectUri = `${process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'http://localhost:3000'}/api/dropbox/callback`;
-  
-  const authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}`;
-  
-  res.status(200).json({ authUrl });
-}
