eff32f1384b03c6917ba3d686c3d0fb024f363bb
jaeo88 <jaeo88@users.noreply.github.com>
2025-10-18 00:45:55 +0900

    Fix Dropbox redirect_uri

--- api/dropbox/auth/start.js
+++ api/dropbox/auth/start.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const clientId = process.env.DROPBOX_APP_KEY;
-  const redirectUri = `${process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'http://localhost:3000'}/api/dropbox/callback`;
+  const redirectUri = 'https://targeted-time.vercel.app/api/dropbox/callback';
   
   const authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}`;
   
