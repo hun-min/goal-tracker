7a96dcc6cf156896dae717700f63fdfa4a431d8c
jaeo88 <jaeok88@gmail.com>
2025-10-25 02:43:15 +0900

    Add token_access_type=offline to get refresh tokens from Dropbox OAuth

--- api/dropbox/auth/start.js
+++ api/dropbox/auth/start.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const clientId = process.env.DROPBOX_APP_KEY;
   const redirectUri = `${req.headers['x-forwarded-proto'] || 'https'}://${req.headers.host}/api/dropbox/callback`;
   
-  const authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}`;
+  const authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&token_access_type=offline`;
   
   res.status(200).json({ authUrl });
 }
